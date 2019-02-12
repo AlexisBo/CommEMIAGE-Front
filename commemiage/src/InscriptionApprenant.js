54e0bff973f1dff55bb749bb6b9670c3e1d850e7
AlexisBo <aboisne77@gmail.com>
2019-02-12 11:13:35 +0100

    formulaire inscription apprenant

--- commemiage/src/InscriptionApprenant.js
+++ commemiage/src/InscriptionApprenant.js
@@ -28,24 +28,43 @@ class InscriptionApprenant extends Component {
                     <form onSubmit={this.handleSubmit}>
                         <div class="panel-body">
 
-                            <label htmlFor="nom">Saisir le nom du module</label>
+                            <label htmlFor="nom">Nom :</label>
                             <input type="text" class="form-control" id="nom" name="nom"/>
                             
-                            <label htmlFor="coefficient">Saisir le coefficient</label>
-                            <input type="text" class="form-control" id="coefficient" name="coefficient"/>
+                            <label htmlFor="prenom">Prénom :</label>
+                            <input type="text" class="form-control" id="prenom" name="prenom"/>
                             
-                            <label htmlFor="seuil">Saisir seuil de rattrapage</label>
-                            <input type="text" class="form-control" id="seuil" name="seuil"/>                        
+                            <label htmlFor="adresse">Adresse postale :</label>
+                            <input type="text" class="form-control" id="adresse" name="adresse"/>
+
+                            <label htmlFor="mail">Adresse mail :</label>
+                            <input type="text" class="form-control" id="mail" name="mail"/>
+
+                            <label htmlFor="dateForm">Date de formation :</label>
+                            <input type="text" class="form-control" id="dateForm" name="dateForm"/>
+
+                            <label htmlFor="filiere">Filière :</label>
+                            <select class="form-control" id="filiere" name="filiere">
+                                <option value="filiere1">Filière 1</option>
+                                <option value="filiere2">Filière 2</option>
+                                <option value="filiere3">Filière 3</option>
+                            </select>
+
+                            <label htmlFor="module">Modules :</label>
+                            <select multiple class="form-control" id="module" name="module">
+                                <option value="module1">Module 1</option>
+                                <option value="module2">Module 2</option>
+                                <option value="module3">Module 3</option>
+                                <option value="module4">Module 4</option>
+                            </select>
                         </div>
                         <div class="panel-footer">
-                            <button type="submit" class="btn btn-primary" style={{ marginTop: 10+'px' }}>Création module </button>
+                            <button type="submit" class="btn btn-primary" style={{ marginTop: 10+'px' }}>S'inscrire </button>
                         </div>
                     </form>
                 </div>
             </div>
         )
-            
-
     }
 }
 
