663968c24e9756ff6d227a322ed09d2bd6374f66
FakihD <fakih.darkaoui@hotmail.com>
2019-03-28 11:18:39 +0100

    Commit anti-conflit

--- commemiage/src/Login.js
+++ commemiage/src/Login.js
@@ -1,61 +1,52 @@
 import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+//import 'bootstrap/dist/css/bootstrap.min.css';
 class Login extends Component {
-
-    constructor(props) {
-        super(props);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.state = {
-            email: '',
-            password : ''
-        }
+   constructor(props) {
+       super(props);
+       this.handleSubmit = this.handleSubmit.bind(this);
+       this.state = {
+           email: '',
+           password : ''
+       }
+   }
+   handleSubmit(event) {
+       event.preventDefault();
+       console.log('check data',this.state);
+       console.log('check data json',JSON.stringify({
+           email : this.state.email,
+           password : this.state.password
+       }));
+       fetch('http://localhost:3010/utilisateurs/login/'+ this.state.email + "/" + this.state.password)
+       .then((resp) => resp.json())
+       .then(function(data) {
+           console.log("login rep - data" + JSON.stringify(data));
+           localStorage.setItem('user_role', data.role);
+           localStorage.setItem('user_email', data.email);
+           localStorage.setItem('user_token', data.token);
+           window.location.reload();
+       });
     }
-    
-    handleSubmit(event) {
-        event.preventDefault();
-        
-        console.log('check data',this.state);
-        console.log('check data json',JSON.stringify({
-            email : this.state.email,
-            password : this.state.password
-        }));
-
-        fetch('http://localhost:3010/utilisateurs/login/'+ this.state.email + "/" + this.state.password)
-        .then((resp) => resp.json())
-        .then(function(data) {
-            console.log("login rep - data" + JSON.stringify(data));
-            localStorage.setItem('user_role', data.role); 
-            localStorage.setItem('user_email', data.email); 
-            localStorage.setItem('user_token', data.token); 
-            window.location.reload();          
-        });
-     }
-
-    render() {
-
-        return (
-            <div className="login col-md-6">
-                <div className="panel panel-default" style={{ border: "1px solid grey", padding: 10 + 'px'}}>
-                    <div className="panel-header">
-                        <h4 className="title">Connexion</h4>
-                    </div>
-                    <form onSubmit={this.handleSubmit}>
-                        <div className="panel-body">
-                            <label htmlFor="mail">Adresse mail :</label>
-                            <input type="text" value={this.state.email} onChange={(ev)=>this.setState({email:ev.target.value})} className="form-control" id="email" name="email"/>
-                            
-                            <label htmlFor="password">Mot de passe :</label>
-                            <input type="password" value={this.state.password} onChange={(ev)=>this.setState({password:ev.target.value})} className="form-control" id="password" name="password"/>
-                        </div>
-                        <div className="panel-footer">
-                            <button type="submit" className="btn btn-primary" style={{ marginTop: 10+'px' }}>Login </button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        )
-    }
+   render() {
+       return (
+           <div className="login col-md-6">
+               <div className="card">
+                   <div className="card-header">
+                       <h4 className="title">Connexion</h4>
+                   </div>
+                   <form onSubmit={this.handleSubmit}>
+                       <div className="card-body">
+                           <label htmlFor="mail">Adresse mail :</label>
+                           <input type="text" value={this.state.email} onChange={(ev)=>this.setState({email:ev.target.value})} className="form-control" id="email" name="email"/>
+                           <label htmlFor="password">Mot de passe :</label>
+                           <input type="password" value={this.state.password} onChange={(ev)=>this.setState({password:ev.target.value})} className="form-control" id="password" name="password"/>
+                       </div>
+                       <div className="card-footer">
+                           <button type="submit" className="btn btn-primary" style={{ marginTop: 10+'px' }}>Login </button>
+                       </div>
+                   </form>
+               </div>
+           </div>
+       )
+   }
 }
-
 export default Login
--- commemiage/src/PathAdministrateur.js
+++ commemiage/src/PathAdministrateur.js
@@ -7,52 +7,52 @@ import NewTuteur from "./NewTuteur";
 import InscriptionApprenant from "./InscriptionApprenant";
 
 function deconnexion () {
-  localStorage.clear();
-  window.location.reload();
+ localStorage.clear();
+ window.location.reload();
 }
 
 const Path = () => (
-  <Router>
-    <div>
-      <ul className="list-inline">
-        <li className="list-inline-item">
-          <u style={{color: "blue"}}><Link to="/">Home</Link></u>
-        </li>
-        <li className="list-inline-item">
-          <u style={{color: "blue"}}><Link to="/creationfiliere">Création filière</Link></u>
-        </li>
-        <li className="list-inline-item">
-          <u style={{color: "blue"}}><Link to="/creationmodule">Création module</Link></u>
-        </li>
-        <li className="list-inline-item">
-          <u style={{color: "blue"}}><Link to="/inscriptionapprenant">S'inscrire</Link></u>
-        </li>
-        <li className="list-inline-item">
-          <u style={{color: "blue"}}><Link to="/newtuteur">Création Tuteur</Link></u>
-        </li>
-        <li className="list-inline-item">
-          <u style={{color: "blue"}}><Link to="/attributiontuteur">Attribution tuteur</Link></u>
-        </li>
-        <li className="list-inline-item">
-          <u style={{color: "blue"}}><Link to="" onClick={deconnexion}>Logout</Link></u>
-        </li>
-      </ul>
-      <hr />
+ <Router>
+   <div>
+     <ul className="list-inline">
+       <li className="list-inline-item">
+         <u style={{color: "blue"}}><Link to="/">Home</Link></u>
+       </li>
+       <li className="list-inline-item">
+         <u style={{color: "blue"}}><Link to="/creationfiliere">Création filière</Link></u>
+       </li>
+       <li className="list-inline-item">
+         <u style={{color: "blue"}}><Link to="/creationmodule">Création module</Link></u>
+       </li>
+       <li className="list-inline-item">
+         <u style={{color: "blue"}}><Link to="/inscriptionapprenant">S'inscrire</Link></u>
+       </li>
+       <li className="list-inline-item">
+         <u style={{color: "blue"}}><Link to="/newtuteur">Création Tuteur</Link></u>
+       </li>
+       <li className="list-inline-item">
+         <u style={{color: "blue"}}><Link to="/attributiontuteur">Attribution tuteur</Link></u>
+       </li>
+       <li className="list-inline-item">
+         <u style={{color: "blue"}}><Link to="" onClick={deconnexion}>Logout</Link></u>
+       </li>
+     </ul>
+     <hr />
 
-      <Route exact path="/" component={Home} />
-      <Route path="/creationmodule" component={CreationModule} />
-      <Route path="/creationfiliere" component={CreationFiliere} />
-      <Route path="/attributiontuteur" component={AttributionTuteur} />
-      <Route path="/newtuteur" component={NewTuteur} />
-      <Route path="/inscriptionapprenant" component={InscriptionApprenant} />
-    </div>
-  </Router>
+     <Route exact path="/" component={Home} />
+     <Route path="/creationmodule" component={CreationModule} />
+     <Route path="/creationfiliere" component={CreationFiliere} />
+     <Route path="/attributiontuteur" component={AttributionTuteur} />
+     <Route path="/newtuteur" component={NewTuteur} />
+     <Route path="/inscriptionapprenant" component={InscriptionApprenant} />
+   </div>
+ </Router>
 );
 
 const Home = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
+ <div>
+   <h2>Home</h2>
+ </div>
 );
 
 export default Path;
